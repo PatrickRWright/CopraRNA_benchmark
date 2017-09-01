fec809ddd031bdd042fb391870a4f38ea07db872
Patrick R. Wright <wrightp@informatik.uni-freiburg.de>
2017-09-01 15:42:06 +0200

    changing line styles

--- plot_performance.R
+++ plot_performance.R
@@ -31,12 +31,12 @@ pdf("CopraRNA2_benchmark.pdf")
     # CopraRNA 1
     plot(c1_ranks_cumulated, type="l", main="", ylab="# True Positives", xlab="# Target predictions per sRNA", ylim=c(0,120), lwd=2, col="coral")
     # IntaRNA
-    lines(intarna_ranks_cumulated, col="seagreen3", lwd=2)
+    lines(intarna_ranks_cumulated, col="gray48", lwd=2)
     # CopraRNA 2 ooi
     lines(c2_ooi_rank_cumulated, col="cornflowerblue", lwd=2)
     # CopraRNA 2 evo
-    lines(c2_evo_rank_cumulated, col=1, lwd=2)
+    lines(c2_evo_rank_cumulated, col="cornflowerblue", lwd=2, lty=2)
 
-    legend(120, 40, c("CopraRNA 1", "CopraRNA 2 ooi", "CopraRNA 2 evo", "IntaRNA"), col=c("coral","cornflowerblue",1,"seagreen3"), lty=c(1,1,1,1), lwd=2)
+    legend(120, 40, c("CopraRNA 1", "CopraRNA 2 ooi", "CopraRNA 2 evo", "IntaRNA"), col=c("coral","cornflowerblue","cornflowerblue","gray48"), lty=c(1,1,2,1), lwd=2)
 dev.off()
 
