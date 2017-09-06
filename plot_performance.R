6ba131b372fe112a1f50e8b0c1bfd8f46f551757
Patrick R. Wright <wrightp@informatik.uni-freiburg.de>
2017-09-06 12:00:33 +0200

    more adjustments for targetrna2

--- plot_performance.R
+++ plot_performance.R
@@ -1,4 +1,5 @@
 d <- read.table("benchmark.csv", sep=";", header=T)
+d_t2 <- read.table("benchmark_targetrna2.csv", sep=";", header=T)
 
 # CopraRNA 1
 c1_ranks <- d$c1_rank
@@ -17,7 +18,7 @@ intarna_ranks <- d$intarna
 intarna_ranks_cumulated <- c()
 
 # TargetRNA2
-targetrna_ranks <- d$targetrna2
+targetrna_ranks <- d_t2$targetrna2
 targetrna_ranks_cumulated <- c()
 
 for (i in 1:200) {
@@ -36,15 +37,15 @@ for (i in 1:200) {
 pdf("CopraRNA2_benchmark.pdf")
     # CopraRNA 1
     plot(c1_ranks_cumulated, type="l", main="", ylab="# True Positives", xlab="# Target predictions per sRNA", ylim=c(0,120), lwd=2, col="coral")
+    # TargetRNA 2
+    lines(targetrna_ranks_cumulated, col="darkkhaki", lwd=2)
     # IntaRNA
     lines(intarna_ranks_cumulated, col="gray48", lwd=2)
     # CopraRNA 2 ooi
     lines(c2_ooi_rank_cumulated, col="cornflowerblue", lwd=2)
     # CopraRNA 2 evo
     lines(c2_evo_rank_cumulated, col="cornflowerblue", lwd=2, lty=2)
-    # TargetRNA 2
-    lines(targetrna_ranks_cumulated, col=2, lwd=2)
 
-    legend(120, 40, c("CopraRNA 2 ooi", "CopraRNA 2 evo", "CopraRNA 1", "IntaRNA", "TargetRNA2"), col=c("cornflowerblue","cornflowerblue","coral","gray48",2), lty=c(1,2,1,1,1), lwd=2)
+    legend(120, 40, c("CopraRNA 2 ooi", "CopraRNA 2 evo", "CopraRNA 1", "IntaRNA", "TargetRNA2"), col=c("cornflowerblue","cornflowerblue","coral","gray48","darkkhaki"), lty=c(1,2,1,1,1), lwd=2)
 dev.off()
 
