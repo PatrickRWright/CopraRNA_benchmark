0ac9d4e84099d7fa68c87c0a679038cc2e7f7749
Patrick R. Wright <wrightp@informatik.uni-freiburg.de>
2017-10-26 14:14:21 +0200

    changed prediction call

--- prediction_call_CopraRNA.sh
+++ prediction_call_CopraRNA.sh
@@ -1,1 +1,5 @@
-./CopraRNA2.pl -cop1 -ntup 200 -ntdown 100 -region 5utr -enrich 200 -topcount 200 -cores 1 -verbose -noclean -websrv -srnaseq file.fasta
+# cop1
+CopraRNA2.pl -cop1 -ntup 200 -ntdown 100 -region 5utr -enrich 200 -topcount 300 -cores 4 -websrv -verbose -srnaseq file.fa
+# cop2
+CopraRNA2.pl -ntup 200 -ntdown 100 -region 5utr -enrich 200 -topcount 300 -cores 4 -websrv -verbose -srnaseq file.fa
+
