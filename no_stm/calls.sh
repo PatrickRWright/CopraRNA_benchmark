6a03c81d22dda06006941ed8b6c812f4a2e9d705
Patrick R. Wright <wrightp@informatik.uni-freiburg.de>
2017-10-27 14:28:31 +0200

    full benchmark

--- no_stm/calls.sh
+++ no_stm/calls.sh
@@ -6,13 +6,13 @@
 # manual curation of specific lines related to additional homologs
 
 # b0592 // additional homolog of b4290
-sed -i 's/RyhB;b0592;fepB;;;/RyhB;b0592;fepB;411;599;1409/g' benchmark_no_stm.csv
+sed -i 's/RyhB;b0592;fepB;;;;;;;;;/RyhB;b0592;fepB;;;272;284;;;;;/' benchmark_no_stm.csv
 # b1531 // additional homolog of b4062
-sed -i 's/RyhB;b1531;marA;;;/RyhB;b1531;marA;470;86;44/g' benchmark_no_stm.csv
+sed -i 's/RyhB;b1531;marA;;;;;;;;;/RyhB;b1531;marA;207;77;;277;46;21;38;;/' benchmark_no_stm.csv
 # b1588 // additional homolog of b0894
-sed -i 's/RyhB;b1588;ynfF;;;/RyhB;b1588;ynfF;188;4;6/g' benchmark_no_stm.csv
+sed -i 's/RyhB;b1588;ynfF;;;;;;;;;/RyhB;b1588;ynfF;230;4;5;83;6;9;9;;/' benchmark_no_stm.csv
 # b1612 // additional homolog of b4122
-sed -i 's/RyhB;b1612;fumA;;;/RyhB;b1612;fumA;88;74;135/g' benchmark_no_stm.csv
+sed -i 's/RyhB;b1612;fumA;;;;;;;;;31/RyhB;b1612;fumA;;71;27;22;138;52;83;;31/' benchmark_no_stm.csv
 
 # plot ROC-like // in no_stm
 R --slave -f plot_performance_no_stm.R
